a084a22e332b8fe4e629d8764b418773f2c405d1
galih <test@test.com>
2024-11-10 14:53:01 +0700

    adding cors, config cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 import swagger from "@elysiajs/swagger";
 import { Elysia } from "elysia";
 import { brandRoutes } from "./features/brands/brand.routes";
+import { cors } from "@elysiajs/cors";
 
 const app = new Elysia()
   .use(swagger())
+  .use(
+    cors({
+      origin: "http://127.0.0.1:5173", // Svelte dev server
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+    })
+  )
   .get("/", () => "Hello Elysia")
   .group("/api", (app) => app.use(brandRoutes))
   .listen(3000);
